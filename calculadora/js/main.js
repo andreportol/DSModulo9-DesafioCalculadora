a1d42e2fdf7338a96171e3decdbb24a15ec8d322
andreportol <andreportol@gmail.com>
2022-11-30 10:14:28 -0400

    Alteração de tipo nos atributos input_1, input_2 e total.

--- calculadora/js/main.js
+++ calculadora/js/main.js
@@ -1,30 +1,66 @@
 function somar(){
-    const input_1 = parseFloat(document.getElementById("number1").value);
-    const input_2 = parseFloat(document.getElementById("number2").value);
+    let input_1 = parseFloat(document.getElementById("number1").value);
+    let input_2 = parseFloat(document.getElementById("number2").value);
     let total = 0 ;
     if(isNumber(input_1) & (isNumber(input_2))){
         total = input_1 + input_2;
         document.getElementById("result-box").innerHTML = total;
-    }   
+        input_1 = document.getElementById("number1").classList.remove('input-error');
+        input_2 = document.getElementById("number2").classList.remove('input-error');
+
+    } else if(!isNumber(input_1)){
+        let input_1 = document.getElementById("number1").classList.add('input-error');
+        document.getElementById("result-box").innerHTML = "0";
+        if(!isNumber(input_2)) {
+            let input_2 = document.getElementById("number2").classList.add('input-error');
+            document.getElementById("result-box").innerHTML = "0";
+        }         
+    }
+    else if(!isNumber(input_2)){
+        let input_2 = document.getElementById("number2").classList.add('input-error');
+        document.getElementById("result-box").innerHTML = "0";      
+    }
 }
 function multiplicar(){
-    const input_1 = parseFloat(document.getElementById("number1").value);
-    const input_2 = parseFloat(document.getElementById("number2").value);
+    let input_1 = parseFloat(document.getElementById("number1").value);
+    let input_2 = parseFloat(document.getElementById("number2").value);
     let total = 0 ;
     if(isNumber(input_1) & (isNumber(input_2))){
         total = input_1 * input_2;
         document.getElementById("result-box").innerHTML = total;
-    }   
+        input_1 = document.getElementById("number1").classList.remove('input-error');
+        input_2 = document.getElementById("number2").classList.remove('input-error');
+
+    } else if(!isNumber(input_1)){
+        let input_1 = document.getElementById("number1").classList.add('input-error');
+        document.getElementById("result-box").innerHTML = "0";
+        if(!isNumber(input_2)) {
+            let input_2 = document.getElementById("number2").classList.add('input-error');
+            document.getElementById("result-box").innerHTML = "0";
+        }         
+    }
+    else if(!isNumber(input_2)){
+        let input_2 = document.getElementById("number2").classList.add('input-error');
+        document.getElementById("result-box").innerHTML = "0";
+        if(isNumber(input_1)){
+            input_1 = document.getElementById("number1").classList.remove('input-error');
+        }      
+    } 
 }
 
 function limparCampos(){
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
-    document.getElementById("result-box").innerHTML = "0"
+    document.getElementById("result-box").innerHTML = "0";
+    input_1 = document.getElementById("number1").classList.remove('input-error');
+    input_2 = document.getElementById("number2").classList.remove('input-error');
+
 }
 
 function isNumber(n){
     return !isNaN(parseFloat(n)) && isFinite(n);   
 }
+
+
 
 
